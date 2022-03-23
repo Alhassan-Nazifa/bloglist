62d92d0085128a034569d8b4c73bc0316e93eb4e
Alhassan-Nazifa <naalhassan174@gmail.com>
2022-03-23 04:08:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from 'react'
 
 function App() {
+  const [title, setTitle]=useState("");
+  const [body, SetBody]=useState("");
+  const [author, SetAuthor]=useState("Ama");
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const blog ={title, body, author};
+    console.log(blog);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='create'>
+    <h2>Add a New Blog</h2>
+
+    <form onSubmit={handleSubmit}>
+    <label>Blog Title:</label>
+   <input type="text"required value={title} onChange={(e)=>setTitle(e.target.value)}/>
+    <label>Blog Body:</label>
+   <textarea required value={body} onChange = {(e)=>SetBody(e.target.value)}></textarea>
+
+   <label>Blog Author</label>
+<select value={author} onChange ={(e)=>SetAuthor(e.target.value)}>
+<option value="Ama">Ama</option>
+<option value="Kojo">kojo</option>
+<option value="Abena">Abena</option>
+</select>
+<button>Add Blog</button>
+  
+    </form>
+       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
